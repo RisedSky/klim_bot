cc30ecff7a8f0991cc027e5ad12c67ee03ca7799
RisedSky <risedsky@gmail.com>
2018-09-08 23:48:17 +0200

    Changed the roles for the design

--- commands/roles.js
+++ commands/roles.js
@@ -10,16 +10,7 @@ module.exports = {
             //.setAuthor(`Les rôles vous permettrons d'accéder aux différents salons de vos jeux favoris.`, call.bot.user.avatarURL)
             .setAuthor(`Les commandes vous permettrons d'accéder aux différents salons de jeux.`, call.bot.user.avatarURL)
             //.setDescription(call.bot.invisible_emote + "\n")
-            .setDescription(`${call.bot.invisible_emote}\nCounter-Strike : ${pfx} csgo     |     Call of Duty : ${pfx} cod\n\nRainbow Six Siège : ${pfx} r6s     |     Fortnite : ${pfx} ftn\n\nPlayerunknown's Battlegrounds : ${pfx} pubg     |     League of Legends : ${pfx} lol\n\nOverWatch : ${pfx} ow     |     World of WarCraft : ${pfx} wow\n\nHearthStone : ${pfx} hs`)
-            //.addField("--------------------------", call.bot.rolesArray)
-            
-            /*
-            .addField("Counter-Strike", `${pfx} csgo`, true)
-            .addField("Counter-Strike", `${pfx} csgo`, true)
-            .addBlankField()
-            .addField("Counter-Strike", `${pfx} csgo`, true)
-            .addField("Counter-Strike", `${pfx} csgo`, true)
-            */
+            .setDescription(`${call.bot.invisible_emote}\nPôle Battle Royale :\n:small_orange_diamond: Section Fortnite :arrow_right: ${pfx} ftn\n:small_orange_diamond: Section Playerunknown's Battlegrounds :arrow_right: ${pfx} pubg\n\nPôle FPS :\n:small_orange_diamond: Section Counter-Strike :arrow_right: ${pfx} csgo\n:small_orange_diamond: Section Call of Duty :arrow_right: ${pfx} cod\n:small_orange_diamond: Section Rainbow Six Siège :arrow_right: ${pfx} r6s\n\nPôle MMO-MOBA : \n:small_orange_diamond: Section League of Legends :arrow_right: ${pfx} lol\n:small_orange_diamond: Section Overwatch :arrow_right: ${pfx} ow\n:small_orange_diamond: Section World of Warcraft :arrow_right: ${pfx} wow\n:small_orange_diamond: Section HearthStone :arrow_right: ${pfx} hs`)
 
             .setColor("GREEN")
         call.message.channel.send(embed)
