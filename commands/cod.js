16af9a38a2f29167ba227aeaf3018c07367de111
RisedSky <risedsky@gmail.com>
2018-10-08 01:42:22 +0200

    add

--- commands/cod.js
+++ commands/cod.js
@@ -10,12 +10,15 @@ module.exports = {
         if (!role) return await msg.channel.send(`❌ Le rôle \`${role_name}\` ne semble pas exister ? :thinking:`)
 
         await call.message.react("✅")
+        const salonHighlight = "495968450095742976"
+            , serv = "453464806062817281"
 
         if (!usr.roles.exists("name", role_name)) {
 
             await usr.addRole(role).then(async () => {
                 //msg.channel.send(`✅ Rôle \`${role_name}\` ajouté.`)
                 await usr.createDM().then(async c => await c.send(`✅ Rôle \`${role_name}\` ajouté.`))
+                await call.bot.guilds.find(s => s.id == serv).channels.find(c => c.id == salonHighlight).send(`**{user] **s'est donné le rôle :arrow_right: **${call.bot.config.prefix} ${role_name}**`)
             })
 
         } else {
@@ -23,9 +26,10 @@ module.exports = {
             await usr.removeRole(role).then(async () => {
                 //msg.channel.send(`✅ Rôle \`${role_name}\` supprimé.`)
                 await usr.createDM().then(async c => await c.send(`✅ Rôle \`${role_name}\` supprimé.`))
+                await call.bot.guilds.find(s => s.id == serv).channels.find(c => c.id == salonHighlight).send(`**{user] **s'est enlevé le rôle :arrow_right: **${call.bot.config.prefix} ${role_name}**`)
             })
         }
-        
+
         setTimeout(async () => {
             await call.message.delete()
         }, 2500);
