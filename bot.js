b85be906546ea7b4897f3bbeeea365b612759f0c
RisedSky <risedsky@gmail.com>
2018-10-07 18:12:12 +0200

    fix da sheate

--- bot.js
+++ bot.js
@@ -311,35 +311,6 @@ bot.on("message", async (message) => {
 
     }
 
-
-    //salon mon-setup-klim //364683334644793345
-    //salons #screenshots
-    if (message.channel.id == "364683334644793345" || message.channel.name == "vos-screenshots") {
-        var regex = new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)
-        //if(message.attachments.size < 0 || !message.content.match(regex)) return await message.delete()
-
-        if (!message.attachments.size > 0) {
-            log("1")
-            await message.delete()
-        } else {
-            return;
-        }
-
-        /*
-        if (!message.content.match(regex)) {
-            log("2")
-            await message.delete()
-        }
-        */
-        /* else if () {
-            return await message.delete()
-        } else if () {|| message.attachments.first().client == undefined
-            return await message.delete()
-        }
-        */
-    }
-
-
     if (message.content.startsWith(prefix) && !message.author.bot) {
         try {
             //#region Permission Du Bot
@@ -381,6 +352,33 @@ bot.on("message", async (message) => {
             console.log("Erreur bot :286")
             console.error(error)
         }
+    }
+    
+    //salon mon-setup-klim //364683334644793345
+    //salons #screenshots
+    if (message.channel.id == "364683334644793345" || message.channel.name == "vos-screenshots") {
+        var regex = new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)
+        //if(message.attachments.size < 0 || !message.content.match(regex)) return await message.delete()
+
+        if (!message.attachments.size > 0) {
+            log("1")
+            await message.delete()
+        } else {
+            return;
+        }
+
+        /*
+        if (!message.content.match(regex)) {
+            log("2")
+            await message.delete()
+        }
+        */
+        /* else if () {
+            return await message.delete()
+        } else if () {|| message.attachments.first().client == undefined
+            return await message.delete()
+        }
+        */
     }
 });
 
