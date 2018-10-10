ec312a545d512d06bd08ca0543da657e53e0918b
RisedSky <risedsky@gmail.com>
2018-10-11 01:41:45 +0200

    test

--- commands/warn.js
+++ commands/warn.js
@@ -22,47 +22,50 @@ module.exports = {
             }
 
             //var user_warned = message.guild.members.find("id", call.args[0])
-            var user_warned = call.bot.users.find(u => u.id == call.args[0])
-            if (!user_warned) {
-                console.log("pas trouvé")
-                return message.reply(`Je n'ai pas trouvé l'utilisateur '\`${call.args[0]}\`'`)
-            } else {
-                console.log("trouvé")
+            //var user_warned = call.bot.users.find(u => u.id == call.args[0])
+            //var user_warned = call.bot.guilds.member.guild.fetchMember(, true)
 
-                user_warned.createDM()
-                    .then(c => {
-                        let embed_warning = new Discord.RichEmbed()
-                            .setColor("#FFFF00")
-                            .setDescription(`:warning: Vous avez reçu un avertissement par ${call.bot.GetUserMention(message.author.id)}\n\nPour la raison suivante :\`\`\`${call.content.split(call.args[0])[1]}\`\`\``)
-                            .setTimestamp()
+            call.bot.fetchUser(call.args[0], true)
+                .then(user_warned => {
+                    console.log("trouvé")
 
-                        c.send(embed_warning)
-                        /*
-                        c.send(`:warning: Vous avez reçu un avertissement par '${call.bot.GetUserMention(message.author.id)}' - '${message.author.id}' :warning:\nPour la raison suivante: \`\`\`${call.content.split(call.args[0])[1]}\`\`\` `)
-                        */
-                    })
-                message.react("✅").then(() => {
-                    call.bot.deleteUserMessage(message, 10 * 1000)
+                    user_warned.createDM()
+                        .then(c => {
+                            let embed_warning = new Discord.RichEmbed()
+                                .setColor("#FFFF00")
+                                .setDescription(`:warning: Vous avez reçu un avertissement par ${call.bot.GetUserMention(message.author.id)}\n\nPour la raison suivante :\`\`\`${call.content.split(call.args[0])[1]}\`\`\``)
+                                .setTimestamp()
+
+                            c.send(embed_warning)
+                            /*
+                            c.send(`:warning: Vous avez reçu un avertissement par '${call.bot.GetUserMention(message.author.id)}' - '${message.author.id}' :warning:\nPour la raison suivante: \`\`\`${call.content.split(call.args[0])[1]}\`\`\` `)
+                            */
+                            message.react("✅").then(() => {
+                                call.bot.deleteUserMessage(message, 10 * 1000)
+                            })
+
+                            //const category_name = String(message.channel.parent.name).toLowerCase() //Toujours en lowercase
+                            let logs_channel = "495968450095742976"
+                            let serv = "453464806062817281"
+
+                            let salon = call.bot.guilds.find("id", serv).channels.find("id", logs_channel)
+
+                            var warn_embed = new Discord.RichEmbed()
+                                .setColor("#FFFF00")
+                                .setDescription(`:warning: L'utilisateur ${call.bot.GetUserMention(call.args[0])} a reçu un avertissement par ${call.bot.GetUserMention(message.author.id)} dans le salon : <#${message.channel.id}>\n\nPour la raison suivante : \`\`\`${call.content.split(call.args[0])[1]}\`\`\` `)
+                                .setTimestamp()
+                            salon.send(warn_embed)
+
+                        })
+                        .catch(error => {
+                            console.log("Erreur sur le warn")
+                            console.log(error)
+                        })
                 })
-                    .catch(error => {
-                        console.log("Erreur sur le warn")
-                        console.log(error)
-                    })
-            }
+                .catch(e => {
+                    console.log("pas trouvé")
+                    return message.reply(`Je n'ai pas trouvé l'utilisateur '\`${call.args[0]}\`' (${e.message})`)
+                })
         }
-
-        const category_name = String(message.channel.parent.name).toLowerCase() //Toujours en lowercase
-        let logs_channel = "495968450095742976"
-        let serv = "453464806062817281"
-
-        let salon = call.bot.guilds.find("id", serv).channels.find("id", logs_channel)
-
-        var warn_embed = new Discord.RichEmbed()
-            .setColor("#FFFF00")
-            .setDescription(`:warning: L'utilisateur ${call.bot.GetUserMention(call.args[0])} a reçu un avertissement par ${call.bot.GetUserMention(message.author.id)} dans le salon : <#${message.channel.id}>\n\nPour la raison suivante : \`\`\`${call.content.split(call.args[0])[1]}\`\`\` `)
-            .setTimestamp()
-        salon.send(warn_embed)
-
-
     }
 }
