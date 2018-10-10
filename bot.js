f5c3b61f9ca1ffa3064cdfa6a40d36ac5286eca7
RisedSky <risedsky@gmail.com>
2018-10-10 02:44:08 +0200

    upp

--- bot.js
+++ bot.js
@@ -124,7 +124,7 @@ bot.on("guildMemberRemove", async member => {
             //.setAuthor(`${member.user.tag} a été kick manuellement par ${kick.executor.tag}`)
             .setDescription(`L'utilisateur ${bot.GetUserMention(kick.target.id)} a été kick manuellement par ${bot.GetUserMention(kick.executor.id)}\n\nPour la raison suivante :\n\`\`\`${kick.reason}\`\`\``)
 
-        await bot.guilds.find("id", serv).channels.find("id", "495968450095742976").send(kick_embed)
+        if (member.guild.id == "364679913707667461") await bot.guilds.find("id", serv).channels.find("id", "495968450095742976").send(kick_embed)
     })
 })
 
@@ -204,14 +204,16 @@ bot.on("guildMemberAdd", async member => {
         .setColor("GREEN")
         .setAuthor(`Bienvenue sur le serveur discord KLIM !`, bot.avatarURL)
         .setDescription(`Avant toute chose merci de lire ce message, puis ensuite d'aller lire le <#364687962916651010>\n${bot.invisible_emote}`)
-        .addField(`Pour accéder à vos salons favoris.`, `:small_orange_diamond: Faites la commande : **${bot.config.prefix} roles** dans le salon <#453483219770277888> cela va vous permettre d'accéder à vos différentes sections de jeux.`)
-        .addField(`${bot.invisible_emote}\nLes salons vocaux privés de KLIM.`, `:small_orange_diamond: Vous avez accès à des salons privés sur notre serveur discord.\n` +
+        .addField(`Pour accéder à vos salons favoris.`, `:small_orange_diamond: Faites la commande : **${bot.config.prefix} "vos jeux"** dans le salon <#453483219770277888> cela va vous permettre d'accéder à vos différentes sections de jeux.\n${bot.invisible_emote}`)
+        .addField(`Les salons vocaux privés de KLIM.`, `:small_orange_diamond: Vous avez accès à des salons privés sur notre serveur discord.\n` +
             `Avec ces salons vous allez pouvoir communiquer avec des personnes ou avec vos amis directement sur le serveur officiel de KLIM!\n\n` +
-            ":small_orange_diamond: Rejoignez le salon \"créer votre salon privé\" de votre section de jeu, attendez quelques secondes et le tour est joué !\n\n" +
-            `:information_source: Notez que vous allez avoir la permission de kicker ou bannir les personnes ** de votre salon **.\n\n` +
-            `**Voici quelques points sur ces permissions **: \n` +
-            `:small_orange_diamond: Lorsque vous mutez quelqu'un avec le "Serveur muet" ("Server Mute"), le bot va détecter ce mute et va alors kicker la personne **de votre salon**.\n ` +
+            `:small_orange_diamond: Rejoignez le salon "créer votre salon privé" de votre section de jeu, attendez quelques secondes et le tour est joué !\n${bot.invisible_emote}\n` + `:warning: Si votre jeu ne se trouve dans aucune des sections disponibles, allez dans la section "KLIM eSport" rejoindre "autres jeux", un salon privé sera automatiquement crée.\n${bot.invisible_emote}`)// +
+
+        .addField(`:information_source: Notez que vous allez avoir la permission de kicker ou bannir les personnes ainsi que de modifier **votre salon**.`, //\n\n
+            `${bot.invisible_emote}\n**Voici quelques points sur ces permissions **: \n` +
+            `:small_orange_diamond: Lorsque vous mutez quelqu'un avec le "Serveur muet" ("Server Mute"), le bot va détecter ce mute et va alors kicker la personne **de votre salon**.\n\n` +
             `:small_orange_diamond: Si cette personne en question revient dans votre salon, mutez le avec le "Serveur sourd" ("Server Deafen"), il sera alors "banni" de votre salon **jusqu'à sa suppression**.\n\n` +
+            `:small_orange_diamond: Vous pouvez changer le nombre de slot si vous en avez l'envie en faisant clique droit (sur votre salon) puis "modifier le salon".\n` +
             "*A savoir que votre salon privé sera supprimé dans un délai de 05 minutes si aucune personne n'y est présente.*")
     //.addBlankField()
     usr.createDM()
--- commands/salons.js
+++ commands/salons.js
@@ -22,14 +22,22 @@ module.exports = {
         var embed_salons = new Discord.RichEmbed()
             .setColor("GREEN")
             .setAuthor("Comment créer votre propre salon privé ?", call.bot.user.avatarURL)
-            .setDescription(
-                ":small_orange_diamond: Rejoignez le salon \"créer votre salon privé\" de votre section de jeu, attendez quelques secondes et le tour est joué !\n\n" +
-                `:information_source: Notez que vous allez avoir la permission de kicker ou bannir les personnes **de votre salon**.\n\n ` +
-                `**Voici quelques points sur ces permissions**: \n` +
-                `:small_orange_diamond: Lorsque vous mutez quelqu'un avec le "Serveur muet" ("Server Mute"), le bot va détecter ce mute et va alors kicker la personne **de votre salon**.\n ` +
+            .setDescription(`${call.bot.invisible_emote}\n` +
+                `:small_orange_diamond: Rejoignez le salon \"créer votre salon privé\" de votre section de jeu, attendez quelques secondes et le tour est joué !\n\n`+
+                `:warning: Si votre jeu ne se trouve dans aucune des sections disponibles, allez dans la section "KLIM eSport" rejoindre "autres jeux", un salon privé sera automatiquement crée.\n${call.bot.invisible_emote}`)
+            .addField(`:information_source: Notez que vous allez avoir la permission de kicker ou bannir les personnes ainsi que de modifier **votre salon**.\n`, //\n\n
+                `${call.bot.invisible_emote}\n**Voici quelques points sur ces permissions **: \n` +
+                `:small_orange_diamond: Lorsque vous mutez quelqu'un avec le "Serveur muet" ("Server Mute"), le bot va détecter ce mute et va alors kicker la personne **de votre salon**.\n\n` +
                 `:small_orange_diamond: Si cette personne en question revient dans votre salon, mutez le avec le "Serveur sourd" ("Server Deafen"), il sera alors "banni" de votre salon **jusqu'à sa suppression**.\n\n` +
-                "*A savoir que votre salon privé sera supprimé dans un délai de 5 minutes si aucune personne n'y est présente.*"
-            )
+                `:small_orange_diamond: Vous pouvez changer le nombre de slot si vous en avez l'envie en faisant clique droit (sur votre salon) puis "modifier le salon".\n` +
+                "*A savoir que votre salon privé sera supprimé dans un délai de 05 minutes si aucune personne n'y est présente.*")
+        /*
+        `:information_source: Notez que vous allez avoir la permission de kicker ou bannir les personnes **de votre salon**.\n\n ` +
+        `**Voici quelques points sur ces permissions**: \n` +
+        `:small_orange_diamond: Lorsque vous mutez quelqu'un avec le "Serveur muet" ("Server Mute"), le bot va détecter ce mute et va alors kicker la personne **de votre salon**.\n ` +
+        `:small_orange_diamond: Si cette personne en question revient dans votre salon, mutez le avec le "Serveur sourd" ("Server Deafen"), il sera alors "banni" de votre salon **jusqu'à sa suppression**.\n\n` +
+        "*A savoir que votre salon privé sera supprimé dans un délai de 5 minutes si aucune personne n'y est présente.*"
+        */
 
         await call.message.author.createDM().then(async c => { c.send(embed_salons) })
 
