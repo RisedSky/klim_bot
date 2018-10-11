2ff232f56af311950a6e4fae2054b99870d5a99f
RisedSky <risedsky@gmail.com>
2018-10-11 03:21:37 +0200

    aa

--- commands/ban.js
+++ commands/ban.js
@@ -20,48 +20,56 @@ module.exports = {
                 return message.reply("❌ Vous devez absolument mettre la raison de votre bannissement.").then(m => { call.bot.deleteUserMessage(m, 10000) })
             }
 
+
+            call.bot.fetchUser(call.args[0], true)
+                .then(user_warned => {
+                    console.log("trouvé")
+
+                    user_warned.createDM()
+                        .then(c => {
+                            var embed_ban = new Discord.RichEmbed()
+                                .setColor("#FF0000")
+                                .setDescription(`:warning: Vous avez été banni du serveur ${call.message.guild.name} par ${call.bot.GetUserMention(message.author.id)} (${call.message.author.tag})\n\nPour la raison suivante:\`\`\`${call.content.split(call.args[0])[1]}\`\`\``)
+
+                            c.send(embed_ban)
+                            user_warned.ban(({ reason: `${call.content.split(call.args[0])[1]} (demandé par ${message.author.tag})` }))
+
+                            message.react("✅").then(() => {
+                                call.bot.deleteUserMessage(message, 10 * 1000)
+                            })
+
+                            const category_name = String(message.channel.parent.name).toLowerCase() //Toujours en lowercase
+                            let logs_channel = "495968450095742976"
+                            let serv = "453464806062817281"
+
+                            let salon = call.bot.guilds.find("id", serv).channels.find("id", logs_channel)
+
+                            var warn_embed = new Discord.RichEmbed()
+                                .setColor("#FF0000")
+                                .setDescription(`:warning: L'utilisateur ${call.bot.GetUserMention(call.args[0])} a été banni par ${call.bot.GetUserMention(message.author.id)} dans le salon : <#${message.channel.id}>\n\nPour la raison suivante : \`\`\`${call.content.split(call.args[0])[1]}\`\`\` `)
+                                .setTimestamp()
+                            salon.send(warn_embed)
+                        })
+                        .catch(error => {
+                            console.log("Erreur sur le ban")
+                            console.log(error)
+                        })
+                })
+                .catch(e => {
+                    console.log("pas trouvé")
+                    return message.reply(`Je n'ai pas trouvé l'utilisateur \`${call.args[0]}\`' (${e.message})`)
+                })
+
+
             var user_warned = message.guild.members.find("id", call.args[0])
             if (!user_warned) {
-                console.log("pas trouvé")
-                return message.reply(`Je n'ai pas trouvé l'utilisateur \`${call.args[0]}\`'`)
             } else {
-                console.log("trouvé")
 
-                user_warned.createDM()
-                    .then(c => {
-                        var embed_ban = new Discord.RichEmbed()
-                            .setColor("#FF0000")
-                            .setDescription(`:warning: Vous avez été banni par ${call.bot.GetUserMention(message.author.id)} - '${message.author.id}'\n\nPour la raison suivante:\`\`\`${call.content.split(call.args[0])[1]}\`\`\``)
-
-
-                        c.send(embed_ban)
-                        user_warned.ban(({ reason: `${call.content.split(call.args[0])[1]} (demandé par ${message.author.tag})` }))
-                        /*
-                        c.send(`:warning: Vous avez reçu un avertissement par '${call.bot.GetUserMention(message.author.id)}' - '${message.author.id}' :warning:\nPour la raison suivante: \`\`\`${call.content.split(call.args[0])[1]}\`\`\` `)
-                        */
-                    })
-                message.react("✅").then(() => {
-                    call.bot.deleteUserMessage(message, 10 * 1000)
-                })
-                    .catch(error => {
-                        console.log("Erreur sur le ban")
-                        console.log(error)
-                    })
             }
         }
 
 
-        const category_name = String(message.channel.parent.name).toLowerCase() //Toujours en lowercase
-        let logs_channel = "495968450095742976"
-        let serv = "453464806062817281"
-        
-        let salon = call.bot.guilds.find("id", serv).channels.find("id", logs_channel)
 
-        var warn_embed = new Discord.RichEmbed()
-            .setColor("#FF0000")
-            .setDescription(`:warning: L'utilisateur ${call.bot.GetUserMention(call.args[0])} a été banni par ${call.bot.GetUserMention(message.author.id)} dans le salon : <#${message.channel.id}>\n\nPour la raison suivante : \`\`\`${call.content.split(call.args[0])[1]}\`\`\` `)
-            .setTimestamp()
-        salon.send(warn_embed)
 
 
     }
