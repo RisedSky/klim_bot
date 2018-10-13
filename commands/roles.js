a6c620f8802c6e8c10cf790181361f2d203b8bf8
RisedSky <risedsky@gmail.com>
2018-10-13 02:04:05 +0200

    aze

--- commands/roles.js
+++ commands/roles.js
@@ -13,19 +13,20 @@ module.exports = {
             .setAuthor(`Les commandes vous permettrons d'accéder aux différents salons de jeux.`, call.bot.user.avatarURL)
             //.setDescription(call.bot.invisible_emote + "\n")
             .setDescription(`${call.bot.invisible_emote}\n` +
-                `Pôle Battle Royale :` +
-                `\n:small_orange_diamond: Section Fortnite :arrow_right: **${pfx} ftn**` +
-                `\n:small_orange_diamond: Section Playerunknown's Battlegrounds :arrow_right: **${pfx} pubg**` +
-                `\n:small_orange_diamond: Section Call of Duty Blackout :arrow_right: **${pfx} cod**\n\n` +
+                `**__Pôle Battle Royale :__**\n` +
+                `:small_orange_diamond: Section Playerunknown's Battlegrounds :arrow_right: **${pfx} pubg**\n` +
+                `:small_orange_diamond: Section Fortnite :arrow_right: **${pfx} ftn**\n` +
+                `:small_orange_diamond: Section Call of Duty Blackout :arrow_right: **${pfx} cod**\n\n` +
 
-                `Pôle FPS :\n` +
+                `**__Pôle FPS :__**\n` +
                 `:small_orange_diamond: Section Counter-Strike :arrow_right: **${pfx} cs**\n` +
-                `:small_orange_diamond: Section Overwatch :arrow_right: **${pfx} ow**\n` +
-                `:small_orange_diamond: Section Rainbow Six Siège :arrow_right: **${pfx} r6s**\n\n` +
-                `Pôle MMOBA - Sports : \n` +
+                `:small_orange_diamond: Section Rainbow Six Siège :arrow_right: **${pfx} r6s**\n` +
+                `:small_orange_diamond: Section Overwatch :arrow_right: **${pfx} ow**\n\n` +
+
+                `**__Pôle MMOBA - Sports :__**\n` +
+                `:small_orange_diamond: Section World of Warcraft & HearthStone :arrow_right: **${pfx} wow** - **${pfx} hs**\n` +
                 `:small_orange_diamond: Section League of Legends :arrow_right: **${pfx} lol**\n` +
                 `:small_orange_diamond: Section Dota 2 :arrow_right: **${pfx} d2**\n` +
-                `:small_orange_diamond: Section World of Warcraft & HearthStone :arrow_right: **${pfx} wow** - **${pfx} hs**\n` +
                 `:small_orange_diamond: Section Rocket League & FIFA :arrow_right: **${pfx} rl** - **${pfx} fifa**\n`
                 /*
                 `Pôle Sport-Action :\n`+
