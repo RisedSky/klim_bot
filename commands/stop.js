483b8a2f5298fcfc64638dc2035fe8af280e42f0
RisedSky <risedsky@gmail.com>
2018-12-11 23:09:48 +0100

    aze

--- commands/stop.js
+++ commands/stop.js
@@ -12,12 +12,12 @@ module.exports = {
             for (var i = server.queue.length - 1; i >= 0; i--) {
                 server.queue.splice(i, 1);
             }
-            message.guild.voiceConnection.disconnect();
             message.channel.send(`:white_check_mark: Toutes les musiques ont été stoppés dans le salon : \`${message.guild.voiceConnection.channel.name}\` :wave:`).then(async msg => {
+                message.guild.voiceConnection.disconnect();
                 await call.bot.deleteMyMessage(msg, 20 * 1000);
             })
         } else {
-            await message.reply(call.bot.current_lang.Music_No_Music_Playing).then(async msg => {
+            await message.reply(`❌ Aucune musique n'est en cours !`).then(async msg => {
                 await call.bot.deleteMyMessage(msg, 5 * 1000)
             })
         }
